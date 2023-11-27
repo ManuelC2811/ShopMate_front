201d293574114220e38a6043f40d0c511de91397
UNmcastiblancoa <mcastiblancoa@unal.edu.co>
2023-11-26 21:21:18 -0500

    Se habilitó la opción de recuperar un usuario con el correo

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import ProductList, ProductDetail, ProductCreate, ProductUpdate, DeleteView, CustomLoginView, RegisterPage, ProductReorder
 from django.contrib.auth.views import LogoutView
 
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
@@ -14,4 +16,9 @@ urlpatterns = [
     path('product-update/<int:pk>/', ProductUpdate.as_view(), name='product-update'),
     path('product-delete/<int:pk>/', DeleteView.as_view(), name='product-delete'),
     path('product-reorder/', ProductReorder.as_view(), name='product-reorder'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="base/password_reset.html"), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="base/password_reset_sent.html"), name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="base/password_reset_form.html"), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="base/password_reset_complete.html"), name = 'password_reset_complete'),
 ]
--- shopmate/settings.py
+++ shopmate/settings.py
@@ -122,3 +122,12 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'correo@gmail.com'
+EMAIL_HOST_PASSWORD = 'contraseña'
