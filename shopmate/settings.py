2d727298582b91b528f903cb37200af469733c79
Manuel Castiblanco <64421854+UNmcastiblancoa@users.noreply.github.com>
2023-12-04 08:41:27 -0500

    Update settings.py

--- shopmate/settings.py
+++ shopmate/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mcastiblancoa@unal.edu.co'
-EMAIL_HOST_PASSWORD = 'Aa6619437'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
